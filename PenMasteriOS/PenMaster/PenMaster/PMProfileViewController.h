ab88589137159268992b8786a928e161c37aece2
Rebecca Vessal <rebeccaVessal@gmail.com>
2013-01-19 17:21:54 -0500

    Added profile view controller and more art assets

---
+++ PenMasteriOS/PenMaster/PenMaster/PMProfileViewController.h
@@ -0,0 +1,13 @@
+//
+//  PMProfileViewController.h
+//  PenMaster
+//
+//  Created by Luis Bobadilla on 1/19/13.
+//  Copyright (c) 2013 Rebecca Vessal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PMProfileViewController : UIViewController
+
+@end
---
+++ PenMasteriOS/PenMaster/PenMaster/PMProfileViewController.m
@@ -0,0 +1,39 @@
+//
+//  PMProfileViewController.m
+//  PenMaster
+//
+//  Created by Luis Bobadilla on 1/19/13.
+//  Copyright (c) 2013 Rebecca Vessal. All rights reserved.
+//
+
+#import "PMProfileViewController.h"
+
+@interface PMProfileViewController ()
+
+@end
+
+@implementation PMProfileViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self)
+    {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
